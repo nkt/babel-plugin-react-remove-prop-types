ad74390b1894e6228879199d1e2878c5d995cb0b
Nikita Gusakov <dev@nkt.me>
2016-02-23 02:04:28 +0300

    Fix statelecss component checking, fixes #13

--- src/is-stateless-component.js
+++ src/is-stateless-component.js
@@ -1,3 +1,24 @@
+function isJSXPath(path) {
+  const argument = path.get('argument');
+  if (argument.node.type === 'JSXElement') {
+    return true;
+  }
+
+  if (argument.node.type === 'CallExpression') {
+    const callee = argument.get('callee');
+    if (callee.matchesPattern('React.createElement')) {
+      return true;
+    }
+
+    const binding = path.scope.getBinding(callee.node.name);
+    if (binding && isReturningJSXElement(binding.path)) {
+      return true;
+    }
+  }
+
+  return false;
+}
+
 function isReturningJSXElement(rootPath) {
   const {node} = rootPath;
   if (node.init && node.init.body && node.init.body.type === 'JSXElement') {
@@ -7,26 +28,8 @@ function isReturningJSXElement(rootPath) {
   let visited = false;
   rootPath.traverse({
     ReturnStatement(path) {
-      if (visited) {
-        return;
-      }
-
-      const argument = path.get('argument');
-      switch (argument.node.type) {
-        case 'JSXElement':
-          visited = true;
-          break;
-        case 'CallExpression':
-          const {node} = argument.get('callee');
-          if (node.object.name === 'React' && node.property.name === 'createElement') {
-            visited = true;
-          }
-
-          const binding = path.scope.getBinding(node.name);
-          if (binding && isReturningJSXElement(binding.path)) {
-            visited = true;
-          }
-          break;
+      if (!visited) {
+        visited = isJSXPath(path);
       }
     }
   });
